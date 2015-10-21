8724e23ca7adb22c92998fc113ebb2928ad023c2
Bill Whitlow <wbw462@bellsouth.net>
2015-10-21 16:02:38 -0400

    PageSpeed tests at 95/100 mobile and 96/100 Desktop
    Inline css into html,

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,24 +2,24 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-          responsive_images: {
-            dynamic: {
-              options: {
-                optimizatonLevel: 3,
-                progressive: true
-              },
-              files: [{
-                expand: true,
-                cwd: 'img',
-                src: ['**/*.{png,jpg,gif}'],
-                dest: 'dist'
-              }]
-            }
+        imagemin: {
+          dynamic: {
+            options: {
+              loops: 7,
+              progressive: true
+            },
+            files: [{
+              expand: true,
+              cwd: 'img',
+              src: ['**/*.{png,jpg,gif}'],
+              dest: 'dist'
+            }]
           }
-//          compress: {
+       }
+//          responsive_images: {
 //            dynamic: {
 //              options: {
-//                loops: 3,
+//                optimizatonLevel: 7,
 //                progressive: true
 //              },
 //              files: [{
@@ -32,35 +32,17 @@ module.exports = function(grunt) {
 //          }
         });
 
-//var Imagemin = require('imagemin');
-//var imageminJpegRecompress = require('imagemin-jpeg-recompress');
-
-//new Imagemin()
-//    .src('view/images/*.jpg')
-//    .dest('view/rebuildimages')
-//    .use(imageminJpegRecompress({loops: 3}))
-//    .run();
-       // Files array format
-        // files: [
-          // { src: ['src/**/*.js'], dest: 'dist/<%= pkg.name %>.js' },
-          // { src: ['plugins/*.js'], dest: 'dist/plugins.js' },
-        // ]
-      // }
-      // }
-    // }
-  // });
-
   // Load plugins
   grunt.loadNpmTasks('grunt-contrib-imagemin');
-  grunt.loadNpmTasks('grunt-responsive-images');
-//  grunt.loadNpmTasks('grunt-imagemin-jpeg-recompress');
+//  grunt.loadNpmTasks('grunt-responsive-images');
+  // grunt.loadNpmTasks('grunt-imagemin-jpeg-recompress');
 
   // Task lists
   // grunt.registerTask('test', ['imagemin', 'jshint']);
 
 
   // grunt.registerTask('default', ['jshint', 'qunit', 'concat', 'uglify']);
-  grunt.registerTask('default', ['responsive_images']);
+  grunt.registerTask('default', ['imagemin'] );
 
 
 };
