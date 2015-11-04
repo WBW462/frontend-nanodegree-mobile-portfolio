dec6f0fa971711c8aba62e80c282132042f5d11c
Bill Whitlow <wbw462@bellsouth.net>
2015-11-03 20:50:00 -0500

    Working on 60 fps
    Modified .mover in views/style.css, worked on updatePositions.

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,39 +10,18 @@ module.exports = function(grunt) {
             },
             files: [{
               expand: true,
-              cwd: 'img',
-              src: ['**/*.{png,jpg,gif}'],
+              src: ['img/*.{png,jpg,gif}', 'views/images/*.{png,jpg,gif}'],
               dest: 'dist'
             }]
           }
        }
-//          responsive_images: {
-//            dynamic: {
-//              options: {
-//                optimizatonLevel: 7,
-//                progressive: true
-//              },
-//              files: [{
-//                expand: true,
-//                cwd: 'img',
-//                src: ['**/*.{png,jpg,gif}'],
-//                dest: 'dist'
-//              }]
-//            }
-//          }
         });
 
   // Load plugins
   grunt.loadNpmTasks('grunt-contrib-imagemin');
 //  grunt.loadNpmTasks('grunt-responsive-images');
-  // grunt.loadNpmTasks('grunt-imagemin-jpeg-recompress');
 
   // Task lists
-  // grunt.registerTask('test', ['imagemin', 'jshint']);
-
-
   // grunt.registerTask('default', ['jshint', 'qunit', 'concat', 'uglify']);
   grunt.registerTask('default', ['imagemin'] );
-
-
 };
