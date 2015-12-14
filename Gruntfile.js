e6b137bbba5e7b26cbe66003d2e0472a2c748da9
Bill Whitlow <wbw462@bellsouth.net>
2015-12-14 12:16:50 -0500

    Updated files
    Went over each file, made minor adjustments to formatting.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,27 +1,27 @@
 module.exports = function(grunt) {
 
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
         imagemin: {
-          dynamic: {
-            options: {
-              loops: 7,
-              progressive: true
-            },
-            files: [{
-              expand: true,
-              src: ['img/*.{png,jpg,gif}', 'views/images/*.{png,jpg,gif}'],
-              dest: 'dist'
-            }]
-          }
-       }
-        });
+            dynamic: {
+                options: {
+                    loops: 7,
+                    progressive: true
+                },
+                files: [{
+                    expand: true,
+                    src: ['img/*.{png,jpg,gif}', 'views/images/*.{png,jpg,gif}'],
+                    dest: 'dist'
+                }]
+            }
+        }
+    });
 
-  // Load plugins
-  grunt.loadNpmTasks('grunt-contrib-imagemin');
-//  grunt.loadNpmTasks('grunt-responsive-images');
+    // Load plugins
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
+    //  grunt.loadNpmTasks('grunt-responsive-images');
 
-  // Task lists
-  // grunt.registerTask('default', ['jshint', 'qunit', 'concat', 'uglify']);
-  grunt.registerTask('default', ['imagemin'] );
+    // Task lists
+    // grunt.registerTask('default', ['jshint', 'qunit', 'concat', 'uglify']);
+    grunt.registerTask('default', ['imagemin']);
 };
